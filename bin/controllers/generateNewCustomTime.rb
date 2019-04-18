72c129a15cb9512ec943e53c2a457305ce942c3b
M-garrigan <mgarrigan99@gmail.com>
2019-04-18 19:23:08 -0400

    Finished the newCustomTime() fn.
    This function records the time.now, then calls a function to parse the
    time into an array while singling out the minute portion and rounding it
    down to the closest 5 minute increment. An example would be 47 mins would round
    down to 45 minutes and 22 mins would round down to 20 mins.
    
    Also the if statement comparison between time_now_rounded & old_time may be
    somewhat terse so I will talk it through now.
    The statement is '(time_now_rounded - old_time).empty? == true'
    So the overall comparison is a boolean == true
    The (time_now_rounded - old_time).empty? is subtracting two arrays and if all
    the elements values are exactly the same then the array will equal an empty
    array '[]'. So the .empty? method will return true if it is called on an empty
    array.

--- bin/controllers/generateNewCustomTime.rb
+++ bin/controllers/generateNewCustomTime.rb
@@ -1,21 +1,22 @@
-# bin/generateNewCustomTime.rb
+# bin/controllers/generateNewCustomTime.rb
 
 require "date"
 require_relative "../helpers/roundTimeDown.rb"
+require_relative "../models/customTimeModel.rb"
 
 def newCustomTime(conn)
   time_now = DateTime.now
-  puts time_now
 
   # round down current time
   time_now_rounded = roundTimeDown(time_now)
-  puts time_now_rounded
-  puts time_now_rounded.class
+  
   # query db for last time recorded
-  # old_time = retrieveOldCustomTime(dbConnection, cursor)
-
-  # if (time_now_rounded == old_time): 
-  #   return False
-  # else: 
-  #   return time_now_rounded
+  query = 'SELECT time_custom FROM last_custom_time_stamp ORDER BY id DESC LIMIT 1'
+  old_time = retrieveOldCustomTime(conn, query)
+  
+  if ((time_now_rounded - old_time).empty? == true)
+    return nil
+  else
+    return time_now_rounded
+  end
 end
