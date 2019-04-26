2ee5e2cfbb74daf6aa397ea641fc4fbfc479d3ac
M-garrigan <mgarrigan99@gmail.com>
2019-04-26 19:22:36 -0400

    Changed the return value for retrieveOldCustomTime function.
    Before it mutated the original array when converting a string to an
    integer, but now we have a new array that we construct to hold the
    converted values and return that array.

--- bin/controllers/generateNewCustomTime.rb
+++ bin/controllers/generateNewCustomTime.rb
@@ -13,8 +13,8 @@ def newCustomTime(conn)
   # query db for last time recorded
   query = 'SELECT time_custom FROM last_custom_time_stamp ORDER BY id DESC LIMIT 1'
   old_time = retrieveOldCustomTime(conn, query)
-  
-  if ((time_now_rounded - old_time).empty? == true)
+
+  if (time_now_rounded.eql?(old_time))
     return nil
   else
     return time_now_rounded
