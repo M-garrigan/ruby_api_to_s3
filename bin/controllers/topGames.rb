694101181b34768868b83180f91ff3301ce0e2eb
M-garrigan <mgarrigan99@gmail.com>
2019-05-23 17:27:15 -0400

    Removed the functions that checked the static postgres tables.

--- bin/controllers/topGames.rb
+++ bin/controllers/topGames.rb
@@ -3,8 +3,6 @@
 require "httparty"
 require "json"
 require_relative "../helpers/config.rb"
-require_relative "../models/topGamesStaticModel.rb"
-require_relative "../models/topGamesModel.rb"
 
 # -------------------------------------------------------------------------------
 # callTwitchApiForTopGames
@@ -12,8 +10,8 @@ require_relative "../models/topGamesModel.rb"
 # @about -- uses Twitch API v5 Kraken, currently there is also the newer Helix
 #        -- API but it does not contain all the information that I need.
 # 
-# @return {Array or nil} -- if the call is sucessfull we will have an array of 
-#                        -- hash data for each game. 
+# @return {Array} -- if the call is sucessfull we will have an array of 
+#                        -- hash data for each game or an empty array 
 # -------------------------------------------------------------------------------
 
 def callTwitchApiForTopGames()
@@ -28,44 +26,6 @@ def callTwitchApiForTopGames()
   )
   res_parsed = JSON.parse(res.body)
   
-  if (res_parsed['top'].size > 0)
-    return res_parsed['top']
-  else 
-    return nil
-  end
-end
-
-# -------------------------------------------------------------------------------
-# handleTopGames
-# 
-# @about -- top_games_static table is used to store meta info for each game
-#        -- so we want to store that info only once. 
-#
-# @param conn {PG::Connection} -- the postgres connection
-# @param time {Array} -- array of 5 ints,  signature: [yr, mo, day, hr, min]
-#
-# @return {none} -- no explicit return 
-# -------------------------------------------------------------------------------
-      
-def handleTopGames(conn, time)
-  games = callTwitchApiForTopGames()
+  return res_parsed['top']
   
-  if (games) 
-    games.each do |game|
-      
-      game_id = game['game']['_id']
-      query = "SELECT * FROM top_games_static WHERE game_id=#{game_id}"
-      static_game = selectGameIfAvailable(conn, query)
-
-      if (!static_game) 
-        query_static = "INSERT INTO top_games_static (game_id, name, box_art_url, logo_template) VALUES ($1, $2, $3, $4)"
-       
-        insertGameIntoStaticTable(conn, query_static, game)
-      end
-    end
-  # then add data to db with models/topGamesModel.py
-  insertTopGames(conn, games, time) 
-  end 
 end
-
-  
--- bin/controllers/topStreams.rb
+++ bin/controllers/topStreams.rb
@@ -3,8 +3,6 @@
 require "httparty"
 require "json"
 require_relative "../helpers/config.rb"
-require_relative "../models/topStreamsStaticModel.rb"
-require_relative "../models/topStreamsModel.rb"
 
 # -------------------------------------------------------------------------------
 # callTwitchApiForTopStreams
@@ -12,8 +10,8 @@ require_relative "../models/topStreamsModel.rb"
 # @about -- uses Twitch API v5 Kraken, currently there is also the newer Helix
 #        -- API but it does not contain all the information that I need.
 # 
-# @return {Array or nil} -- if the call is sucessfull we will have an array of 
-#                        -- hash data for each stream. 
+# @return {Array} -- if the call is sucessfull we will have an array of 
+#                        -- hash data for each stream or an empty array.
 # -------------------------------------------------------------------------------
 
 def callTwitchApiForTopStreams()
@@ -28,43 +26,8 @@ def callTwitchApiForTopStreams()
   )
   res_parsed = JSON.parse(res.body)
   
-  if (res_parsed['streams'].size > 0)
-    return res_parsed['streams']
-  else 
-    return nil
-  end
-end
-
-# -------------------------------------------------------------------------------
-# handleTopStreams
-# 
-# @about -- top_streams_static table is used to store meta info for each stream
-#        -- so we want to store that info only once. 
-#
-# @param conn {PG::Connection} -- the postgres connection
-# @param time {Array} -- array of 5 ints,  signature: [yr, mo, day, hr, min]
-#
-# @return {none} -- no explicit return 
-# -------------------------------------------------------------------------------
-      
-def handleTopStreams(conn, time)
-  streams = callTwitchApiForTopStreams() # returns array of 20 streams or nil
+  return res_parsed['streams']
   
-  if (streams) 
-    streams.each do |stream|
-      stream_id = stream['channel']['_id'].to_i
-      query = "SELECT * FROM top_streams_static WHERE user_id=#{stream_id}"
-      static_stream = selectStreamIfAvailable(conn, query)
-
-      if (!static_stream) 
-        query_static = "INSERT INTO top_streams_static (user_name, name, language, created_at, logo, url, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7)"
-       
-        insertStreamIntoStaticTable(conn, query_static, stream)
-      end
-    end
-  # then add data to db with models/topStreamsModel.py
-  insertTopStreams(conn, streams, time) 
-  end 
 end
 
   
