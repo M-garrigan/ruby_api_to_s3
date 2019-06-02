e7370fcb8e7bda4e6ccf288b3e33420fd1b0b7d4
M-garrigan <mgarrigan99@gmail.com>
2019-06-02 17:39:18 -0400

    Adding functionality to zip files and removed the controllers dir.
    We now are producing the api data into a .json file then zipping
    that file in order to send it to the S3 bucket. The average file
    size reduction has been from 63 MB to 9 MB.

--- main.rb
+++ main.rb
@@ -3,10 +3,17 @@
 require "aws-sdk-s3"
 require "json"
 require "time"
+require "zip"
 require_relative "bin/helpers/config.rb"
-require_relative "bin/controllers/formatTime.rb"
-require_relative "bin/controllers/topGames.rb"
-require_relative "bin/controllers/topStreams.rb"
+require_relative "bin/formatTime.rb"
+require_relative "bin/topGames.rb"
+require_relative "bin/topStreams.rb"
+
+# TODO
+# 1) make a template for a metadata.zip file to add to the S3 day directory
+#   a) Only add the metadata.zip file on the first entry for the day
+#   b) Then just update the file into an array
+# 2) Fill out README.md file so that it is useful
 
 
 # Entry Point
@@ -20,33 +27,50 @@ def handle_main (event, context)
       secret_access_key: s3Config[:secretAccessKey]
     )
     
-    # generate timestamp, fetch top games & fetch top streams
+    # generate data to be stored in s3 bucket
     time = formatTime()
     games = callTwitchApiForTopGames()
     streams = callTwitchApiForTopStreams()
-
-    # combine all the data into a hash
+    
+    # aggregate data into a hash
     data = {
       time: time,
       games: games,
-      streams: streams
+      streams: streams,
     }
-    # file name will be epoch (in ms) as a string
-    # example "58539349.json"
-    epoch = data[:time][:epoch]
-    fileName = "#{epoch.to_s}.json"
 
-    file = File.new(fileName, "w")
+    # directory & file name variables
+    year = time[:year]
+    month = time[:month]
+    day = time[:day]
+    hour = time[:hour]
+    minute = time[:minute]
+
+    # create .json file to place JSON formatted data
+    file = File.new("#{hour}_#{minute}.json", "w")
     file.puts(JSON.generate(data))
     file.close
     
-    res = s3.put_object({
-      bucket: "watch-me-api-to-s3",
-      key: "api/#{fileName}",
-      body: JSON.generate(data),
-    })
+    # zip the file
+    fileName = "#{hour}_#{minute}.zip"
+    Zip::File.open(File.join(Dir.pwd, fileName), Zip::File::CREATE) do |zipfile|
+      ["#{hour}_#{minute}.json"].each do |file|
+        zipfile.add(file, File.join(Dir.pwd, file))
+      end
+    end
 
-    puts('res: ', res)
+    # send the zipped file to S3
+    # res = s3.put_object({
+    #   bucket: "watch-me-api-to-s3",
+    #   key: "#{year}/#{month}/#{day}/#{fileName}",
+    #   body: JSON.generate(data),
+    # })
+
+    # remove the local .zip & .json files
+    File.delete("#{hour}_#{minute}.json")
+    File.delete("#{hour}_#{minute}.zip")
+   
+    # puts('res: ', res)
   rescue Aws::S3::Errors::ServiceError => err
     puts('S3:Error: ', err)
   end
